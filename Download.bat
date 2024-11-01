918a6d77f7353ca05f253d4ee7c697b314487b95
banditjangkrik <jangkrikbandit@gmail.com>
2024-11-01 18:35:59 +0700

    Update Download.bat

--- Download.bat
+++ Download.bat
@@ -7,7 +7,12 @@ powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%c
 pip install pyautogui --quiet
 choco install vcredist-all --no-progress
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+powershell -Command "Invoke-WebRequest 'https://github.com/banditjangkrik/VM-QuickConfig/raw/refs/heads/main/app/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+
+:: Download IDM installer
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://mirror.internetdownloadmanager.com/idman648build25.exe', 'idman.exe')"
+start /wait idman.exe /silent /norestart
+
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
